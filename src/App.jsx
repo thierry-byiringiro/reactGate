800219e9b348e101d4cf59aaff8c0df464db4b63
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-04-08 16:09:06 +0200

    added the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import Home from "./exercises/Home";
+import About from "./exercises/About";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./exercises/Navbar";
+import Layout from "./exercises/Layout";
 function App() {
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Route>,
+    ),
+  );
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
